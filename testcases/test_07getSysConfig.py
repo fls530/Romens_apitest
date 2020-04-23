1d1874119c9584159546c3b8a7e6f8aaff7b913f
Administrator <530740300@qq.com>
2020-04-23 14:25:56 +0800

    完成系统获取模块

---
+++ testcases/test_07getSysConfig.py
@@ -0,0 +1,48 @@
+import os
+import unittest
+from common.handle_excel import HandleExcel
+from library.myddt import ddt, data
+from common.handle_config import conf
+from requests import request
+from common.handle_logging import log
+from common.handle_path import DATA_DIR
+from common.handle_data import replace_data, login
+
+filename = os.path.join(DATA_DIR, "testcase.xlsx")
+
+
+@ddt
+class syncSysConfigTestCase(unittest.TestCase):
+    excel = HandleExcel(filename, "syncSysConfig")
+    cases = excel.read_data()
+
+    @data(*cases)
+    def test_syncSysConfig(self, case):
+        method = case["method"]
+        headers = eval(conf.get("env", "headers"))
+        url = conf.get("env", "url") + case["url"]
+        data = eval(replace_data(case["data"]))
+        data["UserGuid"] = login()
+        row = case["case_id"] + 1
+        expected = eval(case["expected"])
+        res = (
+            request(
+                url=url,
+                method=method,
+                data=data,
+                headers=headers)).json()
+        try:
+            self.assertEqual(expected["result"], res["result"])
+            self.assertEqual(expected["msg"], res["msg"])
+        except AssertionError as e:
+            # 结果回写excel中
+            log.error("用例--{}--执行未通过".format(case["title"]))
+            log.debug("预期结果：{}".format(expected))
+            log.debug("实际结果：{}".format(res))
+            log.exception(e)
+            self.excel.write_data(row=row, column=8, value="未通过")
+            raise e
+        else:
+            # 结果回写excel中
+            log.info("用例--{}--执行通过".format(case["title"]))
+            self.excel.write_data(row=row, column=8, value="通过")
